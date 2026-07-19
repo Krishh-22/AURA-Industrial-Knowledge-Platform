15d8440ccdbf7cd8747c38e69b2339b5b6c422f6
Krishh-22 <krishnanishad73766@gmail.com>
2026-07-20 00:14:46 +0530

    Implemented text chunking and embedding generation

---
+++ app/services/embedding_service.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+
+class EmbeddingService:
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def generate_embeddings(self, chunks: list[str]):
+        return self.model.encode(chunks).tolist()
+    
+embedding_service=EmbeddingService()
